cc081416fe4d322a59d6efdad6c0cb7349053c8a
OwlRepo <romeoangeles010517@gmail.com>
2024-05-19 02:14:30 +0800

    feat: initial content updates

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Romeo Angeles Jr | Portfolio';
+export const SITE_DESCRIPTION = 'Web portfolio of Romeo Angeles Jr whos a Front End Engineer specializing in web and mobile development by using modern framworks.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
